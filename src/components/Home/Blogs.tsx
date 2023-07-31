464621fc1aa915a91711b4250c91ad7bacf936ba
Matthew Kim <matthewkimcs@gmail.com>
2023-07-30 22:27:18 -0700

    css tweaks

--- src/components/Home/Blogs.tsx
+++ src/components/Home/Blogs.tsx
@@ -62,11 +62,11 @@ export default function Blogs() {
           <h2 className="text-3xl font-bold tracking-tight text-white sm:text-4xl">
             From the blog
           </h2>
-          <p className="mt-2 text-lg leading-8 text-gray-300">
-            Learn how to grow your business with our expert advice.
+          <p className="mt-2 text-lg leading-8 text-gray-200">
+            Read the latest blogs from professional software engineers.
           </p>
         </div>
-        <div className="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-t border-gray-200 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3">
+        <div className="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-t border-white/10 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3">
           {posts.map((post) => (
             <article
               key={post.id}
